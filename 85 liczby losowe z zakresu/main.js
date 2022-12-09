87f09f452ca393aa305a8a7df94db737a76194f8
davyxz <xaex.art@wp.pl>
2022-12-09 10:32:26 +0100

    9.12.2022

--- 85 liczby losowe z zakresu/main.js
+++ 85 liczby losowe z zakresu/main.js
@@ -2,21 +2,15 @@ const wynik = document.querySelector('#wynik');
 const btn = document.querySelector('button');
 const mini = document.querySelector('#mini');
 const maks = document.querySelector('#maks');
-const ilosc = document.querySelector('#ilosc');
+const licz_n = document.querySelector('#licz_n');
 
 btn.addEventListener('click', function() {
 
+    wynik.innerHTML=``
     let max = parseInt(maks.value);
     let min = parseInt(mini.value);
-    let ilosc = parseInt(ilosc.value);
-    let wynik_tekst = " "
-    for (i=0;i<=n;i++){
-        wynik_tekst =+
-    }
-
-    let w1 = Math.floor(Math.random()*(max-min+1)+min);
-    let w2 = Math.floor(Math.random()*(max-min+1)+min);
-    let w3 = Math.floor(Math.random() * (max - min + 1) + min);
-    let w4 = Math.floor(Math.random() * (max - min + 1) + min);
-    let w5 = Math.floor(Math.random() * (max - min + 1) + min);
+    let n = parseInt(licz_n.value);
+   for ( let i=0; i<n;i++){
+       wynik.innerHTML +=`{$Math.floor(Math.random() * (max - min + 1) + min)}`
+   }
 })
