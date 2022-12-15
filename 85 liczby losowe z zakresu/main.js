30faef2913c1a8711d32128bda55c3921917d3db
davyxz <xaex.art@wp.pl>
2022-12-15 08:09:40 +0100

    15.12.2022

--- 85 liczby losowe z zakresu/main.js
+++ 85 liczby losowe z zakresu/main.js
@@ -1,16 +1,15 @@
 const wynik = document.querySelector('#wynik');
 const btn = document.querySelector('button');
-const mini = document.querySelector('#mini');
-const maks = document.querySelector('#maks');
-const licz_n = document.querySelector('#licz_n');
+const liczba_n = document.querySelector("#liczba_n");
+const liczba_min = document.querySelector("#liczba_min");
+const liczba_max = document.querySelector("#liczba_max");
 
-btn.addEventListener('click', function() {
-
-    wynik.innerHTML=``
-    let max = parseInt(maks.value);
-    let min = parseInt(mini.value);
-    let n = parseInt(licz_n.value);
-   for ( let i=0; i<n;i++){
-       wynik.innerHTML +=`{$Math.floor(Math.random() * (max - min + 1) + min)}`
-   }
+btn.addEventListener('click', function () {
+    let min = parseInt(liczba_min.value);
+    let max = parseInt(liczba_max.value);
+    let n = parseInt(liczba_n.value);
+    wynik.innerHTML = "Liczby wylosowane to: ";
+    for (let i = 0; i < n; i++) {
+        wynik.innerHTML += `${Math.floor(Math.random() * (max - min + 1) + min)}`+" ";
+    }
 })
