99444207aec9c5c3185cc30f165bd27b8edd6f81
davyxz <114773903+davyxz@users.noreply.github.com>
2022-12-22 08:46:17 +0100

    Add files via upload

---
+++ T22 funkcje/zadanie_T225/main.js
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+const liczba_a = document.querySelector("#liczba_a");
+const liczba_b = document.querySelector("#liczba_b");
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+function euklides(a, b) {
+    while (a!=b) {
+        if(a>b) a=a-b
+        else b=b-a
+    }
+    return a;
+}
+
+btn.addEventListener('click', function () {
+    a = parseInt(liczba_a.value);
+    b = parseInt(liczba_b.value);
+    wynik.innerHTML = "Największy wspólny dzielnik to: "+euklides(a,b)
+=======
+const liczba_a = document.querySelector("#liczba_a");
+const liczba_b = document.querySelector("#liczba_b");
+const wynik = document.querySelector('#wynik');
+const btn = document.querySelector('button');
+
+function euklides(a, b) {
+        var reszta;
+        while (b) {
+            reszta = a % b;
+            a = b;
+            b = reszta;
+        }
+        return a;
+}
+
+btn.addEventListener('click', function () {
+    a = parseInt(liczba_a.value);
+    b = parseInt(liczba_b.value);
+    wynik.innerHTML = "Największy wspólny dzielnik to: "+euklides(a,b)
+>>>>>>> origin/master
+})
